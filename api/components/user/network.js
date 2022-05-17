e01d093d3e431f675ec497463fa6042e5de41926
luis molina <luismolina_20@hotmail.com>
2022-05-17 09:14:12 -0500

    3er commit Changes made it in class

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,12 +1,32 @@
 //microservicio privado
 const expres = require("express");
 
-const router = express.Router();
+const router = expres.Router();
 
-router.get('/', function (req, res){
-    res.send({
-        success:'si funcia'
-    });
+router.get("/", function (req, res) {
+  res.send({
+    success: "si funcia",
+  });
+});
+
+router.post("/login", function (req, res) {
+  console.log(req.query);
+
+  res.send({
+    username: "",
+    pass: "",
+  });
+});
+
+router.post("/register", function (req, res) {
+  console.log(req.query);
+
+  res.send({
+    success: "1",
+    email: "2",
+    pass: "3",
+    phone: "4",
+  });
 });
 
 module.exports = router;
