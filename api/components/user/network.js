d1c3a3992dac10032c9353aeac186bb77dd957bb
luis molina <luismolina_20@hotmail.com>
2022-05-18 09:56:53 -0500

    Commit made in class 18/05/2022

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,24 +1,23 @@
 //microservicio privado
 const expres = require("express");
+const response = require("../../../network/response.js");
 
 const router = expres.Router();
 
-router.get("/", function (req, res) {
-  res.send({
-    success: "si funcia",
-  });
+router.get("/succes1", function (req, res) {
+  response.success(req, res,"" ,200 );
 });
 
 router.post("/login", function (req, res) {
-    console.log("Usuario");
-    console.log(req.query.username);
-    console.log("Contraseña");
-    console.log(req.query.pass);
+  console.log("Usuario");
+  console.log(req.query.username);
+  console.log("Contraseña");
+  console.log(req.query.pass);
 
   res.send({
-    token:"token",
+    token: "token",
     id_user: "user",
-    succes:"succes"
+    succes: "succes",
   });
 });
 
@@ -26,9 +25,9 @@ router.post("/register", function (req, res) {
   console.log(req.query);
 
   res.send({
-    token:"token",
+    token: "token",
     id_user: "user",
-    succes:"succes"
+    succes: "succes",
   });
 });
 
--- config.js
+++ config.js
@@ -1,5 +1,13 @@
 module.exports = {
-    api:{
-        port:process.env.API_PORT || 3000,
-    }
-}
+  api: {
+    port: process.env.API_PORT || 3000,
+  },
+
+  database: {
+    host: "localhost",
+    port: 5432,
+    password: "232001",
+    username: "root",
+    database: "pruebanode",
+  },
+};
