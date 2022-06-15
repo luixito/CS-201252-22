df3f28fe5a9e20814e10af50f09c7d26d6ee9804
luis daniel molina <55844181+luixito@users.noreply.github.com>
2022-06-14 22:35:54 -0500

    changes commited

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+import { api } from './config.js';
+import user from './api/components/user/network.js';
+
+const app = express();
+
+//ROUTERS micro servicio
+app.use('/api/user', user)
+
+//Ejecucion del servidor
+app.listen(api.port, () => {
+    console.log('Servidor corriendo en el puerto en el puerto =>', api.port)
+}
+);
